eeb18d713f0b5e9a6a342830d3126a50af940456
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-12-08 09:24:56 +0100

    Refactor avl.h to include function prototypes

---
+++ avl.h
@@ -0,0 +1,31 @@
+#ifndef AVL_H
+#define AVL_H
+
+typedef struct {
+    int id;
+    float capacity;
+    float consumption;
+} Station;
+
+typedef struct AVL {
+    Station data;           
+    struct AVL *fg;
+    struct AVL *fd;
+    int equilibre;          
+} AVL;
+
+AVL* creeAVL(Station s);
+AVL* insereAVL(AVL* a, Station s, int *h);
+void traiterInfixe(AVL* a);
+void libererAVL(AVL* a);
+
+AVL* rotationGauche(AVL* a);
+AVL* rotationDroite(AVL* a);
+AVL* rotationDoubleGauche(AVL* a);
+AVL* rotationDoubleDroite(AVL* a);
+AVL* equilibrerAVL(AVL* a);
+
+#endif
+
+
+  
