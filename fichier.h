6638f32893eaac542a034cee67329873d9a9c619
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-12-16 14:06:38 +0100

    Update fichier.h

--- fichier.h
+++ fichier.h
@@ -1,1 +1,12 @@
+#ifndef CSV_READER_H
+#define CSV_READER_H
 
+#include <stdio.h>
+#include "avl.h"
+
+FILE* abrirFichierCSV(char* cheminFichier);
+void fermerFichierCSV(FILE* fichier);
+int lireLigneSuivante(FILE* fichier, Station* s);
+int extraireID(char* chaine);
+
+#endif
