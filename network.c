7c01a94a2f3b92a010c5ae25a49de7025c0db33c
taktakos <rytakhim@gmail.com>
2025-12-19 23:17:40 +0100

    Update network.c

--- network.c
+++ network.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-//
-//
+
 // Segment* parse_csv_segment(const char *line) {
 //     // Parsing robuste d'une ligne CSV (séparateur ;)
 //     Segment *seg = calloc(1, sizeof(Segment));
@@ -11,24 +10,59 @@
 //     char *saveptr = NULL;
 //     char *token = strtok_r(tmp, ";", &saveptr);
 //     int col = 0;
-//     while (token) {
-//         switch (col) {
-//             case 0: seg->plant_id = strcmp(token, "-") ? strdup(token) : NULL; break;
-//             case 1: seg->upstream_id = strcmp(token, "-") ? strdup(token) : NULL; break;
-//             case 2: seg->downstream_id = strcmp(token, "-") ? strdup(token) : NULL; break;
-//             case 3: seg->volume = (strcmp(token, "-") && *token) ? atof(token) : 0.0; break;
-//             case 4: seg->leakage = (strcmp(token, "-") && *token) ? atof(token) : 0.0; break;
+//     
+//     while (token != NULL) {
+//         if (col == 0) {
+//             // plant_id
+//             if (strcmp(token, "-") != 0) {
+//                 seg->plant_id = strdup(token);
+//             } else {
+//                 seg->plant_id = NULL;
+//             }
+//         } else if (col == 1) {
+//             // upstream_id
+//             if (strcmp(token, "-") != 0) {
+//                 seg->upstream_id = strdup(token);
+//             } else {
+//                 seg->upstream_id = NULL;
+//             }
+//         } else if (col == 2) {
+//             // downstream_id
+//             if (strcmp(token, "-") != 0) {
+//                 seg->downstream_id = strdup(token);
+//             } else {
+//                 seg->downstream_id = NULL;
+//             }
+//         } else if (col == 3) {
+//             // volume
+//             if (strcmp(token, "-") != 0 && *token != '\0') {
+//                 seg->volume = atof(token);
+//             } else {
+//                 seg->volume = 0.0;
+//             }
+//         } else if (col == 4) {
+//             // leakage
+//             if (strcmp(token, "-") != 0 && *token != '\0') {
+//                 seg->leakage = atof(token);
+//             } else {
+//                 seg->leakage = 0.0;
+//             }
 //         }
+//         
 //         token = strtok_r(NULL, ";", &saveptr);
-//         col++;
+//         col = col + 1;
 //     }
+//     
 //     free(tmp);
 //     return seg;
 // }
 //
 //
 // void free_segment(Segment *seg) {
-//     if (!seg) return;
+//     if (seg == NULL) {
+//         return;
+//     }
+//     
 //     free(seg->plant_id);
 //     free(seg->upstream_id);
 //     free(seg->downstream_id);
